c1b89bad2f23e080d3b40307007cd82b9fd5fdaf
Yehia-Gamal <yahia.elspaa@gmail.com>
2022-06-07 19:07:47 +0200

    eidt file js and css

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let shopping = document.getElementById("shopping");
 let active = document.getElementById("menu");
 
@@ -20,14 +19,15 @@ let nav = document.querySelector(".nav");
 
 burger.onclick = () => {
   nav.classList.toggle("active");
-  burger.classList.toggle("is-active");
+  burger.classList.toggle("is-active")
 };
 
 window.onscroll = () => {
   active.classList.remove("active");
   activeSearch.classList.remove("active");
-  burger.classList.remove("is-active");
+  burger.classList.remove("is-active")
   nav.classList.remove("active");
+
 };
 
 let body = document.querySelector("section");
@@ -36,32 +36,3 @@ body.onclick = () => {
   active.classList.remove("active");
   activeSearch.classList.remove("active");
 };
-=======
-let shopping = document.getElementById("shopping");
-let active = document.getElementById("menu");
-
-shopping.onclick = () => {
-  active.classList.toggle("active");
-  activeSearch.classList.remove("active");
-};
-
-let search = document.getElementById("search");
-let activeSearch = document.getElementById("form");
-
-search.onclick = () => {
-  activeSearch.classList.toggle("active");
-  active.classList.remove("active");
-};
-
-window.onscroll = () => {
-  active.classList.remove("active");
-  activeSearch.classList.remove("active");
-};
-
-let body = document.querySelector("section");
-
-body.onclick = () => {
-  active.classList.remove("active");
-  activeSearch.classList.remove("active");
-};
->>>>>>> af6862999aadd31d372e5f874be6ef0079838bc1
