c5e50c0ab3ac2a4ce47eb71f312fb185d839ebea
Yehia-Gamal <yahia.elspaa@gmail.com>
2022-06-08 09:51:46 +0200

    css and js edit

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ let active = document.getElementById("menu");
 shopping.onclick = () => {
   active.classList.toggle("active");
   activeSearch.classList.remove("active");
+  burger.classList.remove("is-active");
+  nav.classList.remove("active");
 };
 
 let search = document.getElementById("search");
@@ -12,6 +14,8 @@ let activeSearch = document.getElementById("form");
 search.onclick = () => {
   activeSearch.classList.toggle("active");
   active.classList.remove("active");
+  burger.classList.remove("is-active");
+  nav.classList.remove("active");
 };
 
 let burger = document.getElementById("burger");
@@ -20,6 +24,8 @@ let nav = document.querySelector(".nav");
 burger.onclick = () => {
   nav.classList.toggle("active");
   burger.classList.toggle("is-active");
+  active.classList.remove("active");
+  activeSearch.classList.remove("active");
 };
 
 window.onscroll = () => {
@@ -34,4 +40,6 @@ let body = document.querySelector("section");
 body.onclick = () => {
   active.classList.remove("active");
   activeSearch.classList.remove("active");
+  burger.classList.remove("is-active");
+  nav.classList.remove("active");
 };
